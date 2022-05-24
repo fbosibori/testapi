37d030f87925716fe2831530cc7fc317cb43b2c7
Matara <KEN20676@kcbgroup.com>
2022-05-24 12:09:29 +0300

    CRUD operations

--- src/main/java/com/kcbgroup/testapi/controller/AccountController.java
+++ src/main/java/com/kcbgroup/testapi/controller/AccountController.java
@@ -3,8 +3,12 @@ package com.kcbgroup.testapi.controller;
 import com.kcbgroup.testapi.Repo.AccountRepo;
 import com.kcbgroup.testapi.models.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,49 +16,58 @@ import java.util.List;
 @RequestMapping("api/v1/account/")
 public class AccountController {
 
-   @Autowired
-   AccountRepo accountRepo;
+    @Autowired
+    AccountRepo accountRepo;
 
     @GetMapping
-    public List<Account> getAccounts(){
-        List<Account> accounts = new ArrayList<>();
-        Account faith = new Account(1,"IBAN1", "BNK001", 200);
-        Account john = new Account(2,"IBAN2", "BNK002", 300);
-        accounts.add(faith);
-        accounts.add(john);
+    public List<Account> getAccounts() {
+        List<Account> accounts = accountRepo.findAll();
         return accounts;
     }
-    @GetMapping(path="{accountid}")
-    public Account getAccount(@PathVariable("accountid") int accountid){
-        List<Account> accounts = new ArrayList<>();
-        Account faith = new Account(1,"IBAN1", "BNK001", 200);
-        Account john = new Account(2,"IBAN2", "BNK002", 300);
-        accounts.add(faith);
-        accounts.add(john);
-        return faith;
+
+    @GetMapping(path = "{accountId}")
+    public ResponseEntity<?> getAccount(@PathVariable("accountId") int accountId) {
+        try {
+            Account account = accountRepo.getById(accountId);
+            Account acc = new Account(account.getAccountId(), account.getIban(), account.getBankCode(), account.getCustomerId());
+            return new ResponseEntity<>(acc, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
+
     @PostMapping
-    public String addAccount(@RequestBody Account accountToAdd){
+    public String addAccount(@RequestBody Account accountToAdd) {
         accountRepo.save(accountToAdd);
 
         String iban = accountToAdd.getIban();
-        String msg = "Account "+iban+" created successfully";
-        return msg;
-
-    }
-    @PutMapping(path="{accountid}")
-    public String updateAccount(@RequestBody Account AccountToAdd, @PathVariable("accountid") int accountid){
-        //TODO: Add data to db
-        String msg = "Account "+accountid+" updated successfully";
-        return msg;
-
-    }
-    @DeleteMapping(path="{accountid}")
-    public String deleteAccount(@PathVariable("accountid") int accountid){
-        //TODO:Delete accountid
-        String msg = "Account "+accountid+" deleted successfully";
+        String msg = "Account " + iban + " created successfully";
         return msg;
 
     }
 
+    @PutMapping(path = "{accountId}")
+    @Transactional
+    public ResponseEntity<?> updateAccount(@PathVariable("accountId") int accountId, @RequestBody Account newAccountDetails) {
+        try {
+            Account account = accountRepo.getById(accountId);
+            account.setBankCode(newAccountDetails.getBankCode());
+            account.setIban(newAccountDetails.getIban());
+
+            return new ResponseEntity<>("Account details updated successfully.", HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path = "{accountId}")
+    public String deleteAccount(@PathVariable("accountId") int accountId) {
+        if (accountRepo.existsById(accountId)) {
+            accountRepo.deleteById(accountId);
+            return "Account deleted successfully";
+        }
+        return "Account " + accountId + " does not exist";
+
+    }
 }
+
